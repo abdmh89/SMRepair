e0e916f7d2f31aee52de9f1cd5f9a06071d54e08
abdmh89 <abdmh89@gmail.com>
2015-11-19 19:39:47 -0500

    Update RVPs.java

--- simModel/RVPs.java
+++ simModel/RVPs.java
@@ -34,18 +34,107 @@ class RVPs
 	    return(nxtInterArr+model.getClock());
 	}
 	
-	//DuType1000CallArrival()
+//DuType1000CallArrival()
 	private int DuType1000CallArrival(){
-		if(0 <= model.getClock() < 60){
+		if(0 <= UDP.getScaledClock() < 60){
 			// return next arrival time with mean 60/7
 		}
-		else if(60 <= model.getClock() < 120){
-			//return next arriva
+		else if(60 <= UDP.getScaledClock() < 120){
+			//return next arriva time with mean 60/12
+		
+		else if(120 <= UDP.getScaledClock() < 180){
+			//return next arriva time with mean 60/10
+		else if(180 <= UDP.getScaledClock() < 240){
+			//return next arriva time with mean 60/7
+		else if(240 <= UDP.getScaledClock() < 300){
+			//return next arriva time with mean 60/5
+		else if(300 <= UDP.getScaledClock() < 360){
+			//return next arriva time with mean 60/4
+		else if(360 <= UDP.getScaledClock() < 420){
+			//return next arriva time with mean 60/5
+		else if(420 <= UDP.getScaledClock() < 480){
+			//return next arriva time with mean 60/4
+		else if(480 <= UDP.getScaledClock() < 540){
+			//return next arriva time with mean 60/3		
 		}
-	}
-	
+		
+	//DuType2000CallArrival()
 	private int DuType2000CallArrival(){
+		if(0 <= UDP.getScaledClock() < 60){
+			// return next arrival time with mean 60/8
+		}
+		else if(60 <= UDP.getScaledClock() < 120){
+			//return next arriva time with mean 60/11
+		
+		else if(120 <= UDP.getScaledClock() < 180){
+			//return next arriva time with mean 60/8
+		else if(180 <= UDP.getScaledClock() < 240){
+			//return next arriva time with mean 60/9
+		else if(240 <= UDP.getScaledClock() < 300){
+			//return next arriva time with mean 60/6
+		else if(300 <= UDP.getScaledClock() < 360){
+			//return next arriva time with mean 60/4
+		else if(360 <= UDP.getScaledClock() < 420){
+			//return next arriva time with mean 60/3
+		else if(420 <= UDP.getScaledClock() < 480){
+			//return next arriva time with mean 60/3
+		else if(480 <= UDP.getScaledClock() < 540){
+			//return next arriva time with mean 60/2		
+		}	
+		
 		
 	}
+	
+      //DuType3000CallArrival()
+	private int DuType1000CallArrival(){
+		if(0 <= UDP.getScaledClock() < 60){
+			// return next arrival time with mean 60/5
+		}
+		else if(60 <= UDP.getScaledClock() < 120){
+			//return next arriva time with mean 60/6
+		
+		else if(120 <= UDP.getScaledClock() < 180){
+			//return next arriva time with mean 60/5
+		else if(180 <= UDP.getScaledClock() < 240){
+			//return next arriva time with mean 60/4
+		else if(240 <= UDP.getScaledClock() < 300){
+			//return next arriva time with mean 60/3
+		else if(300 <= UDP.getScaledClock() < 360){
+			//return next arriva time with mean 60/3
+		else if(360 <= UDP.getScaledClock() < 420){
+			//return next arriva time with mean 60/2
+		else if(420 <= UDP.getScaledClock() < 480){
+			//return next arriva time with mean 60/2
+		else if(480 <= UDP.getScaledClock() < 540){
+			//return next arriva time with mean 60/1		
+		}	
+	}
+	//DuType4000CallArrival()
+	private int DuType1000CallArrival(){
+		if(0 <= UDP.getScaledClock() < 60){
+			// return next arrival time with mean 60/2
+		}
+		else if(60 <= UDP.getScaledClock() < 120){
+			//return next arriva time with mean 60/3
+		
+		else if(120 <= UDP.getScaledClock() < 180){
+			//return next arriva time with mean 60/4
+		else if(180 <= UDP.getScaledClock() < 240){
+			//return next arriva time with mean 60/3
+		else if(240 <= UDP.getScaledClock() < 300){
+			//return next arriva time with mean 60/2
+		else if(300 <= UDP.getScaledClock() < 360){
+			//return next arriva time with mean 60/1
+		else if(360 <= UDP.getScaledClock() < 420){
+			//return next arriva time with mean 60/1
+		else if(420 <= UDP.getScaledClock() < 480){
+			//return next arriva time with mean 60/1
+		else if(480 <= UDP.getScaledClock() < 540){
+			//return next arriva time with mean 60/1		
+		}
+}
+	//uType1000Contract()
+	private int uType1000Contract(){
+
 
 }
