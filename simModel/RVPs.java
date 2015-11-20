13d79b7c098db2dd4ab01add570b0ccdfb9b1703
abdmh89 <abdmh89@gmail.com>
2015-11-19 20:42:56 -0500

    updated RVPs

--- simModel/RVPs.java
+++ simModel/RVPs.java
@@ -34,18 +34,169 @@ class RVPs
 	    return(nxtInterArr+model.getClock());
 	}
 	
-	//DuType1000CallArrival()
+//DuType1000CallArrival()
 	private int DuType1000CallArrival(){
-		if(0 <= model.getClock() < 60){
+		if(0 <= UDP.getScaledClock() < 60){
 			// return next arrival time with mean 60/7
 		}
-		else if(60 <= model.getClock() < 120){
-			//return next arriva
+		else if(60 <= UDP.getScaledClock() < 120){
+			//return next arrival time with mean 60/12
+		
+		else if(120 <= UDP.getScaledClock() < 180){
+			//return next arrival time with mean 60/10
+		else if(180 <= UDP.getScaledClock() < 240){
+			//return next arrival time with mean 60/7
+		else if(240 <= UDP.getScaledClock() < 300){
+			//return next arrival time with mean 60/5
+		else if(300 <= UDP.getScaledClock() < 360){
+			//return next arrival time with mean 60/4
+		else if(360 <= UDP.getScaledClock() < 420){
+			//return next arrival time with mean 60/5
+		else if(420 <= UDP.getScaledClock() < 480){
+			//return next arrival time with mean 60/4
+		else if(480 <= UDP.getScaledClock() < 540){
+			//return next arrival time with mean 60/3		
 		}
-	}
-	
+		
+	//DuType2000CallArrival()
 	private int DuType2000CallArrival(){
+		if(0 <= UDP.getScaledClock() < 60){
+			// return next arrival time with mean 60/8
+		}
+		else if(60 <= UDP.getScaledClock() < 120){
+			//return next arriva time with mean 60/11
+		
+		else if(120 <= UDP.getScaledClock() < 180){
+			//return next arrival time with mean 60/8
+		else if(180 <= UDP.getScaledClock() < 240){
+			//return next arrival time with mean 60/9
+		else if(240 <= UDP.getScaledClock() < 300){
+			//return next arrival time with mean 60/6
+		else if(300 <= UDP.getScaledClock() < 360){
+			//return next arrival time with mean 60/4
+		else if(360 <= UDP.getScaledClock() < 420){
+			//return next arrival time with mean 60/3
+		else if(420 <= UDP.getScaledClock() < 480){
+			//return next arrival time with mean 60/3
+		else if(480 <= UDP.getScaledClock() < 540){
+			//return next arrival time with mean 60/2		
+		}	
+		
 		
 	}
-
+	
+      //DuType3000CallArrival()
+	private int DuType1000CallArrival(){
+		if(0 <= UDP.getScaledClock() < 60){
+			// return next arrival time with mean 60/5
+		}
+		else if(60 <= UDP.getScaledClock() < 120){
+			//return next arrival time with mean 60/6
+		
+		else if(120 <= UDP.getScaledClock() < 180){
+			//return next arrival time with mean 60/5
+		else if(180 <= UDP.getScaledClock() < 240){
+			//return next arrival time with mean 60/4
+		else if(240 <= UDP.getScaledClock() < 300){
+			//return next arrival time with mean 60/3
+		else if(300 <= UDP.getScaledClock() < 360){
+			//return next arrival time with mean 60/3
+		else if(360 <= UDP.getScaledClock() < 420){
+			//return next arrival time with mean 60/2
+		else if(420 <= UDP.getScaledClock() < 480){
+			//return next arrival time with mean 60/2
+		else if(480 <= UDP.getScaledClock() < 540){
+			//return next arrival time with mean 60/1		
+		}	
+	}
+	//DuType4000CallArrival()
+	private int DuType1000CallArrival(){
+		if(0 <= UDP.getScaledClock() < 60){
+			// return next arrival time with mean 60/2
+		}
+		else if(60 <= UDP.getScaledClock() < 120){
+			//return next arrival time with mean 60/3
+		
+		else if(120 <= UDP.getScaledClock() < 180){
+			//return next arrival time with mean 60/4
+		else if(180 <= UDP.getScaledClock() < 240){
+			//return next arrival time with mean 60/3
+		else if(240 <= UDP.getScaledClock() < 300){
+			//return next arrival time with mean 60/2
+		else if(300 <= UDP.getScaledClock() < 360){
+			//return next arrival time with mean 60/1
+		else if(360 <= UDP.getScaledClock() < 420){
+			//return next arrival time with mean 60/1
+		else if(420 <= UDP.getScaledClock() < 480){
+			//return next arrival time with mean 60/1
+		else if(480 <= UDP.getScaledClock() < 540){
+			//return next arrival time with mean 60/1		
+		}
 }
+	//uType1000Contract()
+	private final double PERCENT_BASIC = 0.35;
+	@SuppressWarnings("unused")
+	private final double PERCENT_PREMIUM = 0.65;
+	
+	// method
+	public string uType1000Contract(){ string out
+		double randNum = typeRandGen.nextDouble();
+	
+	if (randNum < PERCENT_BASIC )
+	out= "BASIC"
+	else 
+	out= "PREMIUM"
+	
+        return out;
+	}
+	
+	//uType2000Contract()
+	private final double PERCENT_BASIC = 0.35;
+	@SuppressWarnings("unused")
+	private final double PERCENT_PREMIUM = 0.65;
+	
+	// method
+	public string uType2000Contract(){ string out
+		double randNum = typeRandGen.nextDouble();
+	
+	if (randNum < PERCENT_BASIC )
+	out= "BASIC"
+	else 
+	out= "PREMIUM"
+	
+        return out;
+	}
+	
+	//uType3000Contract()
+	private final double PERCENT_BASIC = 0.25;
+	@SuppressWarnings("unused")
+	private final double PERCENT_PREMIUM = 0.75;
+	
+	// method
+	public string uType3000Contract(){ string out
+		double randNum = typeRandGen.nextDouble();
+	
+	if (randNum < PERCENT_BASIC )
+	out= "BASIC"
+	else 
+	out= "PREMIUM"
+	
+        return out;
+	}
+	
+	//uType4000Contract()
+	private final double PERCENT_BASIC = 0.15;
+	@SuppressWarnings("unused")
+	private final double PERCENT_PREMIUM = 0.85;
+	
+	// method
+	public string uType4000Contract(){ string out
+		double randNum = typeRandGen.nextDouble();
+	
+	if (randNum < PERCENT_BASIC )
+	out= "BASIC"
+	else 
+	out= "PREMIUM"
+	
+        return out;
+	}
