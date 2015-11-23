cc3bae0d314fa9b328dadca09966bc964a6e752f
abdmh89 <abdmh89@gmail.com>
2015-11-23 09:58:47 -0500

    updated seeds

--- simModel/Seeds.java
+++ simModel/Seeds.java
@@ -8,6 +8,11 @@ public class Seeds
 	int seed2;   // comment 2
 	int seed3;   // comment 3
 	int seed4;   // comment 4
+	int seed5;   // comment 5
+	int seed6;   // comment 6
+	int seed7;   // comment 7
+	int seed8;   // comment 8
+	int seed9;   // comment 9
 
 	public Seeds(RandomSeedGenerator rsg)
 	{
@@ -15,5 +20,10 @@ public class Seeds
 		seed2=rsg.nextSeed();
 		seed3=rsg.nextSeed();
 		seed4=rsg.nextSeed();
+		seed5=rsg.nextSeed();
+		seed6=rsg.nextSeed();
+		seed7=rsg.nextSeed();
+		seed8=rsg.nextSeed();
+		seed9=rsg.nextSeed();
 	}
 }
