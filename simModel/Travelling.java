99a6520cde89190ed6e1f24a1f6b6c1f4527d65e
abdmh89 <abdmh89@gmail.com>
2015-11-23 10:00:34 -0500

    Updated Travelling

--- simModel/Travelling.java
+++ simModel/Travelling.java
@@ -1,59 +1,73 @@
+package simModel;
+
 import simulationModelling.ConditionalActivity;
 
-public class Traveling extends ConditionalActivity{
-	private Employee rcEmployee;
-	private Customer icCurrentCall;
-	SMRepair model; / for referencing the model
-	int classId;
-	int employeeId;
-	public Travelling(SMRepair model) {this.model = model;}
-
-	protected static boolean precondition(SMRepair model){
-		int days = model.getClock()/1440;
-		if(UDPs.getAvailableEmployee() != null && (model.getClock - days * 1440) <= 510){
-			return true;
-		}
-		else{
-			return false;
-		}
+public class Traveling extends ConditionalActivity
+{
+	private SMRepair model; // for referencing the model
+	private int classId;
+	private int employeeId;
+	
+	public Traveling(SMRepair model) 
+	{
+		this.model = model;
 	}
 
-	public void startingEvent(){
-		String id = UDP.getAvailableEmployee();
-		String[] splitId = id.split(" ");
-		classId = Integer.parseInt(splitId[0]);
-		employeeId = Integer.parseInt(splitId[1]);
-		model.rEmployee[classId][employeeId].status = "Traveling";
-		model.rEmployee[classId][employeeId].assignedTo = UDP.getNextCall(classId);
+	protected static boolean precondition(SMRepair model)
+	{
+		boolean out = false;
+		
+		if(model.udp.getAvailableEmployee() != null && model.udp.getScaledClock() <= 510)
+		{
+			out = true;
+		}
+		
+		return out;
+	}
+
+	public void startingEvent()
+	{
+		int [] id = model.udp.getAvailableEmployee();
+		classId = id[0];
+		employeeId = id[1];
+		model.employees[classId][employeeId].status = "TRAVELING";
+		model.employees[classId][employeeId].assignedTo = model.udp.getNextCall(classId);
 	}
 
 	@Override
-	protected double duration(){
-		return model.rvp.DuTravelTime();
+	protected double duration()
+	{
+		return model.rvp.uTravelTime();
 	}
 
 	@Override
-	protected void terminatingEvent(){
-		icCurrentCall =  model.rEmployee[classId][employeeId].assignedTo;
-		if(icCurrentCall.contractTType == "Basic"){
-			output.servicedBasicCalls += 1;
-			if(icCurrentCall.arrivalTime - iC.currentCall.timeRequested) <= 1440{
-				output.numSatisfiedBasicCalls +=1;
+	protected void terminatingEvent()
+	{
+		model.employees[classId][employeeId].assignedTo.arrivalTime = model.udp.getScaledClock();
+		Call icCurrentCall =  model.employees[classId][employeeId].assignedTo;
+		
+		if(icCurrentCall.contractType == "BASIC")
+		{
+			model.output.servicedBasicCalls += 1;
+			if(icCurrentCall.arrivalTime - icCurrentCall.timeRequested <= 1440)
+			{
+				model.output.numSatisfiedBasicCalls +=1;
 			}
-			output.basicCustomerSatisfaction = output.numSatisfiedBasicCalls/output.servicedBasicCalls;
-		}else{
-			output.servicedPremiumCalls += 1;
-			if((icCurrentCall.arrivalTime - icCurrentCall.timeRequested) <= 180){
-				output.numSatisfiedBasicCalls += 1;
-			} else if (900 < icCurrentCall.ArrivalTime <= 1080){ //need to verify.
-				output.numSatisfiedPremiumCalls += 1;
-			}
-			output.premiumCustomerSatisfaction = output.numSatisfiedPremiumCalls/output.servicedPremiumCalls;
+			model.output.basicCustomerSatisfaction = model.output.numSatisfiedBasicCalls/model.output.servicedBasicCalls;
 		}
-		model.rEmployee[classId][employeeId].assignedTo = icCurrentCall;
-		model.rEmployee[classId][employeeId].status = "Arrived";
-
-
+		else
+		{
+			model.output.servicedPremiumCalls += 1;
+			if((icCurrentCall.arrivalTime - icCurrentCall.timeRequested) <= 180)
+			{
+				model.output.numSatisfiedPremiumCalls += 1;
+			} 
+			else if (icCurrentCall.arrivalTime > 900 && icCurrentCall.arrivalTime <= 1080)
+			{
+				model.output.numSatisfiedPremiumCalls += 1;
+			}
+			model.output.premiumCustomerSatisfaction = model.output.numSatisfiedPremiumCalls/model.output.servicedPremiumCalls;
+		}
 
 	}
 }
