dc41518aa7e9d378c117c20cd09bd670878d17c8
abdmh89 <abdmh89@gmail.com>
2015-11-23 09:59:38 -0500

    updated servicing

--- simModel/Servicing.java
+++ simModel/Servicing.java
@@ -1,63 +1,73 @@
-import simulationModeling.ExtSequelActivity;
+package simModel;
 
-public class Servicing extends ExtSequelActivity{
+import simulationModelling.ExtSequelActivity;
 
-private Employee rcEmployee;
-	private Customer icCurrentCall;
-	SMRepair model; / for referencing the model
-	int classId;
-	int EmployeeId;
-
-public Servicing(SMRepair model){
-	this.model = model;
-}
-
-public abstract int interruptionPreCond(){
-	int days = model.getClock()/1440;
-	int timeCompleted = icCurrentCall.arrivalTime + RVP.DuServiceTime(icCurrentCall.equipmentType) - days*1440
-	if (icCurrentCall.contractType == "Basic" and timeCompleted > 570){
-		return 1;
-	}
-	return 0;
-}
-
-public abstract void interruptionSCS(int interruptID){}
-	if(interruptID == 1){
-		model.rEmployee[classId][employeeId].numOvertimeHours += 0.5;
-		model.rEmployee[classId][employeeId].status = "Available";
+public class Servicing extends ExtSequelActivity
+{
+	private Employee employee;
+	private Call icCurrentCall;
+	private SMRepair model; // for referencing the model
+	
+	public Servicing(SMRepair model, Employee employee)
+	{
+		this.model = model;
+		this.employee = employee;
+		this.icCurrentCall = employee.assignedTo;
 	}
 
-}
-public void startingEvent(){
-	String id = UDP.getArrivedEmployee();
-		String[] splitId = id.split(" ");
-		classId = Integer.parseInt(splitId[0]);
-		employeeId = Integer.parseInt(splitId[1]);
-	model.rEmployee[classId][employeeId].status = "Servicing"
-	icCurrentCall = model.rEmployee[classId][employeeId].assignedTo;
-}
+	public int interruptionPreCond()
+	{
+		int out = 0;
+		int timeCompleted = model.udp.getScaledTime((int)icCurrentCall.arrivalTime + (int)model.rvp.uServiceTime(icCurrentCall.equipmentType));
+		if (icCurrentCall.contractType == "BASIC" && timeCompleted > 570)
+		{
+			out = 1;
+		}
+		
+		return out;
+	}
 
-@Override
-protected double duration(){
-	return model.rvp.DuServiceTime(icCurrentCall.equipmentType);
-}
+	public void interruptionSCS(int interruptID)
+	{
+		if(interruptID == 1)
+		{
+			employee.numOvertimeHours += 0.5;
+			employee.status = "AVAILABLE";
+		}
+	}
+
+
+	public void startingEvent()
+	{
+		//empty event
+	}
 
 	@Override
-	protected void terminatingEvent(){
-		int days = model.getClock();
-		int timeCompleted = icCurrentCall.arrivalTime + RVP.DuServiceTime(icCurrentCall.EquipmentType) - days*1440;
-		if (icCurrentCall.contractType == Basic){
-			if(540 < timeCompleted < 570){
-				model.rEmployee[classId].[employeeId].numOvertimeHours += (timeCompleted - 540)/60;
+	protected double duration()
+	{
+		return model.rvp.uServiceTime(icCurrentCall.equipmentType);
+	}
+
+	@Override
+	protected void terminatingEvent()
+	{
+		int timeCompleted = (int)model.getClock();
+		if (icCurrentCall.contractType == "BASIC")
+		{
+			if(540 < timeCompleted && timeCompleted < 570)
+			{
+				employee.numOvertimeHours += (timeCompleted - 540)/60;
 			}
 		}
-		else{
-			if(timecompleted > 540){
-				model.rEmployee[classId][employeeId].numOvertimeHours += (timeCompleted - 540)/60;
+		else
+		{
+			if(timeCompleted > 540)
+			{
+				employee.numOvertimeHours += (timeCompleted - 540)/60;
 			}
 		}
-		model.rEmployee[classId][employeeId].status = "Available";
-		model.rEmployee[classId][employeeId].assignedTo == null;
+		employee.status = "AVAILABLE";
+		employee.assignedTo = null;
 	}
 
 }
