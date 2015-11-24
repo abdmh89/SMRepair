b58a89ed2f2e8147d733b66017e3750c5a78aeec
abdmh89 <abdmh89@gmail.com>
2015-11-23 19:05:52 -0500

    update constants 2

--- simModel/Constants.java
+++ simModel/Constants.java
@@ -3,7 +3,17 @@ package simModel;
 class Constants 
 {
 	/* Constants */
-	// Define constants as static
-	// Example: protected final static double realConstant = 8.0;
-
+	protected final static int P1000 = 0;
+	protected final static int P2000 = 1; 
+	protected final static int P3000 = 2; 
+	protected final static int P4000 = 3; 
+	protected final static int B1000 = 4; 
+	protected final static int B2000 = 5; 
+	protected final static int B3000 = 6;  
+	protected final static int B4000 = 7;
+	
+	protected final static int CLASSA = 0;  
+	protected final static int CLASSB = 1;
+	
+	protected final static int LUNCH_DURATION = 60;
 }
